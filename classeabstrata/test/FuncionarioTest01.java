87204a9a6a38ab5d4114600f9c64a2d8af2116da
holandajuan <juanholandagithub@gmail.com>
2023-09-19 22:57:24 -0300

    test

--- classeabstrata/test/FuncionarioTest01.java
+++ classeabstrata/test/FuncionarioTest01.java
@@ -6,7 +6,7 @@ import classeabstrata.dominio.Gerente;
 public class FuncionarioTest01 {
     public static void main(String[] args){
         Gerente gerente = new Gerente("carlos", 5000);
-        Desenvolvedor desenvolvedor = new Desenvolvedor("Juan", 12000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Juaan", 12000);
         System.out.println(gerente);
         System.out.println(desenvolvedor);
     }
